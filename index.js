3df42203b992a9800a2bb457382af3a306bb17e1
CoolAcid <jakendall@gmail.com>
2016-03-24 14:48:37 -0400

    Start of libs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 yaml = require('js-yaml');
 fs   = require('fs');
+bundles = {}
 
 var config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
 
@@ -11,12 +12,19 @@ io.on('connect', function(){
     console.log('Connected'); 
     for (var bundle in config.bundles) {
         if (config.bundles[bundle]) {
-            io.emit('joinRoom', bundle);
+            bundles[bundle] = require('./lib/' + bundle + '.js'); //(io)
+            console.log(bundles[bundle](io));
+            bundles[bundle].start();
+//            io.emit('joinRoom', bundle);
         }
     };
 });
 
 io.on('replicantAssigned', function(data) {
+   console.log('Incoming message:', data);
+});
+
+io.on('replicantChanged', function(data) {
    console.log('Incoming message:', data);
 });
 
