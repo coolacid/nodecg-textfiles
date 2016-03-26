a7ecdf2a1b502f9d60f4c0d70e3f9ea50c5a80db
CoolAcid <jakendall@gmail.com>
2016-03-25 21:42:53 -0400

    Replicants now work, on its way to being a thing

--- index.js
+++ index.js
@@ -15,22 +15,22 @@ io.on('connect', function(){
             bundles[bundle] = require('./lib/' + bundle + '.js');
             bundles[bundle](io);
             console.log(bundles[bundle]);
-//            bundles[bundle](io).start();
-//            io.emit('joinRoom', bundle);
         }
     };
 });
 
 io.on('replicantAssigned', function(data) {
     bundle = data.bundle;
-    if (typeof bundles[bundle] == 'function') { 
-        console.log ("yes") 
+    if (typeof bundles[bundle].replicantAssigned == 'function') { 
+        bundles[bundle].replicantAssigned(data);
     }
-    console.log('Incoming message:', data);
 });
 
 io.on('replicantChanged', function(data) {
-//   console.log('Incoming message:', data);
+    bundle = data.bundle;
+    if (typeof bundles[bundle].replicantChanged == 'function') { 
+        bundles[bundle].replicantChanged(data);
+    }
 });
 
 io.on('disconnect', function(){});
