952e24a0b132b3fd7624cdeca70ce90d686900a9
CoolAcid <jakendall@gmail.com>
2016-03-25 21:24:02 -0400

    Replicants kinda work

--- index.js
+++ index.js
@@ -12,20 +12,25 @@ io.on('connect', function(){
     console.log('Connected'); 
     for (var bundle in config.bundles) {
         if (config.bundles[bundle]) {
-            bundles[bundle] = require('./lib/' + bundle + '.js'); //(io)
-            console.log(bundles[bundle](io));
-            bundles[bundle].start();
+            bundles[bundle] = require('./lib/' + bundle + '.js');
+            bundles[bundle](io);
+            console.log(bundles[bundle]);
+//            bundles[bundle](io).start();
 //            io.emit('joinRoom', bundle);
         }
     };
 });
 
 io.on('replicantAssigned', function(data) {
-   console.log('Incoming message:', data);
+    bundle = data.bundle;
+    if (typeof bundles[bundle] == 'function') { 
+        console.log ("yes") 
+    }
+    console.log('Incoming message:', data);
 });
 
 io.on('replicantChanged', function(data) {
-   console.log('Incoming message:', data);
+//   console.log('Incoming message:', data);
 });
 
 io.on('disconnect', function(){});
